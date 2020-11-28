2a920e6413b7231462e2794741874be3c0e50de0
Nahuel Drobotij <drobotij.n@gmail.com>
2020-11-28 01:13:51 -0300

    solucione un error del views

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ app.set('view engine', '.hbs');
 
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', __dirname + '/views');
 app.use(
     express.urlencoded({
         extended: true
